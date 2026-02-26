912a5201ee4c5a30246b8ec5d89804c1ac670058
Joshua <168519351+JoshuaSurico@users.noreply.github.com>
2026-02-26 11:38:17 +0100

    Setup cloud database configuration

--- models/index.js
+++ models/index.js
@@ -1,25 +1,39 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const path = require('path');
 
-// Connexion à la base de données
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: path.join(__dirname, '../db/database.sqlite'),
-    logging: false
-});
+let sequelize;
 
-// Importation des modèles
+// SI on est sur Render (Postgres)
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    });
+} 
+// SINON on est en local (SQLite)
+else {
+    sequelize = new Sequelize({
+        dialect: 'sqlite',
+        storage: path.join(__dirname, '../db/database.sqlite'),
+        logging: false
+    });
+}
+
 const User = require('./User')(sequelize, DataTypes);
 const WeightEntry = require('./WeightEntry')(sequelize, DataTypes);
 const FoodItem = require('./FoodItem')(sequelize, DataTypes);
 const MealEntry = require('./MealEntry')(sequelize, DataTypes);
 
-// Définition des relations
 User.hasMany(WeightEntry);
 WeightEntry.belongsTo(User);
-
 User.hasMany(MealEntry);
 MealEntry.belongsTo(User);
 
-// Export global
 module.exports = { sequelize, User, WeightEntry, FoodItem, MealEntry };
